630f920143e482730e601d9f36da4672b980fa98
Vixey3 <boskovicj15@gmail.com>
2022-04-14 23:54:53 +0200

    Prva verzija

---
+++ nrt1019.zadatak1.js
@@ -0,0 +1,7 @@
+import {
+    evaluate
+    } from 'mathjs'
+   
+    console.log(evaluate('2 + 3'))   // 5
+    console.log(evaluate('sqrt(25)') )    // 5 inch
+    console.log(evaluate('(2 == 3) == false')  )       
